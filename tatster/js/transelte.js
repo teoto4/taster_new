557204fef87c7bea312beca32154e63dba685874
Revy <162366071+teoto4@users.noreply.github.com>
2024-07-19 23:15:14 +0300

    Add files via upload

---
+++ tatster/js/transelte.js
@@ -0,0 +1,32 @@
+export const languageSelect = {
+  "partner-text": {
+    en: "Restaurateur? Join the Taster Platform and rapidly launch one of our iconic virtual restaurant brands from your existing kitchen!",
+    es: "¿Restaurador? ¡Únase a Taster Platform y lance rápidamente una de nuestras icónicas marcas de restaurantes virtuales desde su cocina actual!",
+    fr: "Restaurateur? Rejoignez la plateforme Taster et lancez rapidement l'une de nos marques de restaurant virtuel emblématiques depuis votre cuisine existante!",
+  },
+  "food-text": {
+    en: "The best of street food at home",
+    es: "Lo mejor de la comida callejera en casa.",
+    fr: "Le meilleur de la street food à la maison",
+  },
+  food_desc_btn: {
+    en: "Taster is a collection of online restaurants dedicated to bringing the best of street food to you.",
+    es: "Taster es una colección de restaurantes en línea dedicados a ofrecerte lo mejor de la comida callejera.",
+    fr: "Taster est une collection de restaurants en ligne dédiés à vous offrir le meilleur de la cuisine de rue.",
+  },
+  partner_button_1: {
+    en: "Become a partner",
+    es: "espania",
+    fr: "france",
+  },
+  partner_button_2: {
+    en: "Learn more",
+    es: "espania",
+    fr: "france",
+  },
+  discover: {
+    en: "Learn more",
+    es: "espania",
+    fr: "france",
+  },
+};
